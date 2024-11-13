d3e4a1c1bc35d85c2796823480851e363070316d
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-11-14 03:18:32 +0500

    Добавил загрузку Events из KudaGo

--- src/main/java/com/fin/spr/services/KudaGoSerivce.java
+++ src/main/java/com/fin/spr/services/KudaGoSerivce.java
@@ -3,7 +3,10 @@ package com.fin.spr.services;
 import com.fin.spr.annotations.LogExecutionTime;
 import com.fin.spr.controllers.payload.LocationPayload;
 import com.fin.spr.models.Category;
+import com.fin.spr.models.Event;
 import com.fin.spr.models.Location;
+import com.fin.spr.models.response.EventResponse;
+import com.fin.spr.models.response.EventsResponse;
 import com.fin.spr.storage.InMemoryStorage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +17,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ScheduledExecutorService;
@@ -37,32 +41,45 @@ import java.util.concurrent.TimeUnit;
 
 @Service
 public class KudaGoSerivce  {
+    private static final Logger logger = LoggerFactory.getLogger(KudaGoSerivce.class);
+    private static final String Fields = "title,dates,price,is_free,location";
+
     @Value("${kudago.api.categories.url}")
     private String CATEGORIES_API_URL;
 
     @Value("${kudago.api.locations.url}")
     private String LOCATIONS_API_URL;
 
-    private static final Logger logger = LoggerFactory.getLogger(KudaGoSerivce.class);
+    @Value("${kudago.api.events.url}")
+    private String EVENTS_API_URL;
 
     @Value("${app.init.schedule-duration}")
     private Duration scheduleDuration;
+
+    @Value("${kudago.limits.events.page_size}")
+    private int PAGE_SIZE;
+
+    @Value("${kudago.limits.events.max_page}")
+    private int MAX_PAGE;
 
     private final RestClient restClient;
     private InMemoryStorage<Category, Integer> categoryStorage;
     private final ExecutorService fixedThreadPool;
     private final ScheduledExecutorService scheduledThreadPool;
     private final LocationService locationService;
+    private final EventService eventService;
 
 
     public KudaGoSerivce(RestClient restClient,
                          InMemoryStorage<Category, Integer> categoryStorage,
                          LocationService locationService,
+                         EventService eventService,
                          @Qualifier("fixedThreadPool") ExecutorService fixedThreadPool,
                          @Qualifier("scheduledThreadPool") ScheduledExecutorService scheduledThreadPool) {
         this.restClient = restClient;
         this.categoryStorage = categoryStorage;
         this.locationService = locationService;
+        this.eventService = eventService;
         this.fixedThreadPool = fixedThreadPool;
         this.scheduledThreadPool = scheduledThreadPool;
     }
@@ -136,6 +153,51 @@ public class KudaGoSerivce  {
     }
 
     private void loadEvents() {
+        List<EventsResponse> allEventsResponse = fetchAllEvents();
+        logger.info("Get " + allEventsResponse.size() + " events pages");
+        if (!allEventsResponse.isEmpty()) {
+            List<EventResponse> eventsToAdd = new ArrayList<>();
+            allEventsResponse.forEach(eventsResponse -> eventsToAdd.addAll(eventsResponse.getResults()));
+            eventService.initEvents(eventsToAdd);
+            logger.info("Events have been successfully initialized.");
+        } else {
+            logger.warn("Received null events.");
+        }
+    }
 
+    private List<EventsResponse> fetchAllEvents(){
+        int page = 1;
+        List<EventsResponse> eventsResponses = new ArrayList<>();
+        while (page <= MAX_PAGE) {
+            var events = fetchEvents(page++);
+            if (events == null) break;
+            eventsResponses.add(events);
+        }
+        return eventsResponses;
+    }
+    private EventsResponse fetchEvents(int page) {
+        logger.info("Get events from page " + page);
+        try {
+            var response = restClient.get()
+                    .uri(uriBuilder -> uriBuilder
+                            .path(EVENTS_API_URL)
+                            .queryParam("page", page)
+                            .queryParam("page_size", PAGE_SIZE)
+                            .queryParam("fields", Fields)
+                            .build())
+                    .retrieve()
+                    .toEntity(EventsResponse.class);
+            if (!(response.getStatusCode().is2xxSuccessful() && response.getBody() != null)) {
+                logger.info("Get nothing from page" + page);
+                return null;
+            }
+            logger.info("Get successfull from page" + page);
+            logger.info("Cnt elements = " + response.getBody().getResults().size());
+            return response.getBody();
+        } catch (Exception ex) {
+            logger.info("(ex) Get nothing from page" + page);
+            logger.error(ex.getMessage());
+            return null;
+        }
     }
 }
