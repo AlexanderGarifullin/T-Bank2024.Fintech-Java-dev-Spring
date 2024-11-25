49ab3b0310cb5e34da5c823e865aa4c09dfb0830
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-11-25 22:29:23 +0500

    Добавил в БД таблицу User и классы для работы с этой таблицей

---
+++ src/main/java/com/fin/spr/models/security/User.java
@@ -0,0 +1,53 @@
+package com.fin.spr.models.security;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+@Builder
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "t_users", schema = "security")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "c_name", nullable = false)
+    private String name;
+
+    @Column(name = "c_login", unique = true, nullable = false)
+    private String login;
+
+    @Column(name = "c_hashed_password", nullable = false)
+    private String hashedPassword;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "c_role", nullable = false)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return hashedPassword;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+}
