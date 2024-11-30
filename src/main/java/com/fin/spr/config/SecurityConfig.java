324e6039fdd62359b4f5d996996d68488b865bc2
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-12-01 02:45:42 +0500

    Добавил actuator

--- src/main/java/com/fin/spr/config/SecurityConfig.java
+++ src/main/java/com/fin/spr/config/SecurityConfig.java
@@ -41,6 +41,7 @@ public class SecurityConfig {
                        .requestMatchers("/api/v1/auth/register", "/api/v1/auth/login").permitAll()
                         .requestMatchers("/swagger-ui/**", "/swagger-resources/*", "/v3/api-docs/**").permitAll()
                         .requestMatchers("/api/v1/places/categories").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
                         .requestMatchers("/endpoint", "/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .sessionManagement(sessionManagementConfigurer ->
